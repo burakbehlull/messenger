3abf6a10a7cac5adb5af8c3eb88e7d9d1bad19fd
burakbehlull <burakyabgu@gmail.com>
2024-09-30 21:33:01 +0300

    feat: Create database connect file

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+function db(){
+	const conn = await mongoose.connect(process.env.MONGO_URI)
+	.then(()=> console.log("Bağlantı başarılı"))
+	.catch((err)=> console.log("Hata: ", err.message))
+	return conn
+}
+module.exports = {
+	db
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 require('dotenv').config()
+require('./config/db').db()
 
 const { dmRoute, messageRoute, authRoute } = require('./routers')
 
