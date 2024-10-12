28530626083dfd0df9a02a6348a9a170b61ae2bc
burakbehlull <burakyabgu@gmail.com>
2024-10-12 03:26:33 +0300

    refactor: Redesign DM functions

--- server/controllers/dmController.js
+++ server/controllers/dmController.js
@@ -1,8 +1,4 @@
-const DM = require('../models/DM')
-const Member = require('../models/Member')
-const Message = require('../models/Message')
-const { generateRandomNumber } = require('../helpers/misc')
-const { ShowDm } = require('../helpers/dm')
+const { ShowDm, CreateDm, GetDmMessages, GetDms } = require('../helpers/dm')
 
 
 
@@ -16,41 +12,10 @@ async function userDmCreate(req, res) {
                 message: 'Kullanıcılar geçersiz',
             })
         }
-
-        const members = await Member.find({ _id: { $in: users } }).select('-password')
-        if (members.length !== users.length) {
-            return res.status(400).json({
-                success: false,
-                message: 'Bir veya daha fazla kullanıcı bulunamadı',
-            })
-        }
-
-        const existingDm = await DM.findOne({ users: { $all: users } })
+        const result = await CreateDm(users)
         
-        if (existingDm) {
-            return res.json({
-                success: true,
-                message: 'DM zaten var',
-                dm: existingDm,
-            })
-        }
-        const dmUsers = members.map((user)=> {
-            return `${user.username},`
-        }) 
-        const randomId = await generateRandomNumber()
-        const newDm = await DM.create({
-            name: `${dmUsers}`,
-            type: "directMessage",
-            users: users,
-            id: randomId,
-            invisible: users
-        })
+        return res.status(result.code).json(result)
 
-        return res.json({
-            success: true,
-            message: 'DM başarıyla oluşturuldu',
-            dm: newDm,
-        })
     } catch (error) {
         console.log('Hata: ', error.message);
         return res.status(500).json({
@@ -64,20 +29,8 @@ async function getDmMessages(req, res) {
     try {
         const { dmId } = req.params
 
-        const dm = await DM.findById(dmId)
-        if (!dm) {
-            return await res.status(404).json({
-                success: false,
-                message: 'DM bulunamadı',
-            })
-        }
-
-        const messages = await Message.find({ dmId: dmId }).sort({ timestamp: 1 })
-
-        return res.json({
-            success: true,
-            messages: messages,
-        });
+        const result = await GetDmMessages(dmId)
+        return res.status(result.code).json(result)
     } catch (err) {
         console.log('Hata: ', err.message);
         return await res.status(500).json({
@@ -91,17 +44,8 @@ async function getDms(req, res) {
     try {
         const { userId } = req.params
 
-        const dms = await DM.find({ invisible: userId }).exec()
-        if (!dms) {
-            return await res.status(404).json({
-                success: false,
-                message: 'DMler bulunamadı',
-            })
-        }
-        return res.json({
-            success: true,
-            dms: dms,
-        })
+        const result = await GetDms(userId)
+        return res.status(result.code).json(result)
     } catch (err) {
         console.log('Hata: ', err.message);
         return await res.status(500).json({
--- server/helpers/dm.js
+++ server/helpers/dm.js
@@ -1,4 +1,7 @@
 const DM = require('../models/DM')
+const Member = require('../models/Member')
+const Message = require('../models/Message')
+const { generateRandomNumber } = require('./misc')
 
 async function ShowDm(dmId, userId, isShow){
     try {
@@ -28,6 +31,104 @@ async function ShowDm(dmId, userId, isShow){
     }
 }
 
+async function CreateDm(users){
+    try {
+        const members = await Member.find({ _id: { $in: users } }).select('-password')
+        
+        if (members.length !== users.length) return {
+            success: false,
+            code: 404,
+            message: 'Bir veya daha fazla kullanıcı bulunamadı',
+        }
+        
+
+        const existingDm = await DM.findOne({ users: { $all: users } })
+        if (existingDm) return {
+            success: true,
+            error: 'DM zaten var',
+            code: 200,
+            data: existingDm,
+        }
+        
+        const dmUsers = members.map((user)=> {
+            return `${user.username},`
+        }) 
+
+        const randomId = await generateRandomNumber()
+        const newDm = await DM.create({
+            name: `${dmUsers}`,
+            type: "directMessage",
+            users: users,
+            id: randomId,
+            invisible: users,
+        })
+
+        return {
+            success: true,
+            message: 'DM başarıyla oluşturuldu',
+            dm: newDm,
+            code: 201
+        }
+
+    } catch(err){
+        return {
+            success: false,
+            message: 'Başarısız',
+            error: err.message
+        }
+    }
+}
+
+async function GetDmMessages(dmId){
+    try {
+        const dm = await DM.findById(dmId)
+        if (!dm) return {
+            success: false,
+            message: 'DM bulunamadı',
+            code: 404
+        }
+
+        const messages = await Message.find({ dmId: dmId }).sort({ timestamp: 1 })
+
+        return {
+            success: true,
+            messages: messages,
+            code: 200
+        }
+    } catch(err){
+        return {
+            success: false,
+            message: 'Hata.',
+            error: err.message,
+            code: 500
+        }
+    }
+}
+
+async function GetDms(userId){
+    try {
+        const dms = await DM.find({ invisible: userId }).exec()
+        if (!dm) return {
+            success: false,
+            message: 'DM bulunamadı',
+        }
+        return {
+            success: true,
+            dms: dms,
+        }
+    } catch(err){
+        return {
+            success: false,
+            message: 'Hata.',
+            error: err.message,
+            code: 500
+        }
+    }
+}
+
 module.exports = {
-    ShowDm
+    ShowDm,
+    CreateDm,
+    GetDmMessages,
+    GetDms
 }
